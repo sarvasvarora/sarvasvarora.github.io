d66a52cce3aabc4c125097e6bbdb195095fa9108
Sarvasv Arora <sarvasvarora@gmail.com>
2021-01-12 20:50:05 +0530

    added script for loading pages faster

---
+++ assets/js/ajax-load.js
@@ -0,0 +1,70 @@
+$(document).on('ready', function(){
+    bindLinks()
+})
+
+$(window).on("popstate", function(e) {
+    NProgress.start()
+    $('title').html(e.originalEvent.state.title)
+    $('#content').html(e.originalEvent.state.content)
+    updateExternals()
+    bindLinks()
+    NProgress.done()
+})
+
+function bindLinks(){
+    $("a[href^='/']").on('click', function(e){
+        // Stop link from activating
+        e.preventDefault()
+
+        // Start the NProgress bar
+        NProgress.start()
+
+        // Get the URL to load
+        url = $(this).attr('href')
+
+        // Send a Get request to the URL
+        $.get(url, function(data){
+            // Get the title of the new page
+            regex = /<title>(.*)<\/title>/g
+            newTitle = regex.exec(data)[1]
+
+            // Set the title to the new title
+            $('title').html(newTitle)
+
+            // Replace the content
+            $('#content').html($(data).find('#content').html())
+
+            // Push a new state to the browser
+            history.pushState({
+                'title': $('title').html(),
+                'content': $('#content').html()
+            }, newTitle, url)
+
+            updateExternals()
+
+            // Make NProgress finish
+            NProgress.done()
+
+            // Re Bind to all the links on the page
+            bindLinks()
+        })
+    })
+}
+
+function updateExternals(){
+    // Update disqus
+    // If there is a disqus_thread on the page?
+    if($('#disqus_thread').length !== 0){
+        // Has Disqus been loaded before
+        if ('undefined' !== typeof DISQUS){
+            // Reset Disqus
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = disqus_identifier
+                    this.page.url = disqus_url
+                }
+            });
+        }
+    }
+}
